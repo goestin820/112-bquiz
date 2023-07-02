08e1a58e8cda0e55c06ab17446b3cd50cb834abf
goestin820 <goestin820@gmail.com>
2023-07-03 00:02:43 +0800

    20230702更新乙級解題流程及老師git程式碼註解

--- web01/Controller/Ad.php
+++ web01/Controller/Ad.php
@@ -3,7 +3,7 @@ include_once "DB.php";
 
 class Ad extends DB
 {
-    // public $header = '動態文字廣告管理';
+    // public $header = '動態文字廣告管理'; 
     public $header='動態文字廣告';
     protected $add_header = '新增動態文字廣告';
     public function __construct()
